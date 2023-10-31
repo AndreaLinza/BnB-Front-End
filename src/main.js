63663ab1f04a4420e5ad65d7135305aff8733dc6
giuliacrr <giulia.corrado02@gmail.com>
2023-10-31 17:41:07 +0100

    Changes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
-import './style.css'
+import './style/general.scss'
 import App from './App.vue'
+import { router } from "./router.js"
 
-createApp(App).mount('#app')
+import "bootstrap/dist/css/bootstrap.css";
+
+createApp(App).use(router).mount('#app');
+
+import "bootstrap/dist/js/bootstrap.js";
---
+++ src/router.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+// import HomePage from "./pages/Home.vue";
+
+// const routes = [
+//   {  //HOME
+//     path: "/",
+//     name: "home",
+//     component: HomePage,
+//   },
+
+//  { //NOT FOUND
+//    path: "/:pathMatch(.*)*",
+//    name: "not-found",
+//    component: NotFoundPage
+//  }
+// ];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export { router };
