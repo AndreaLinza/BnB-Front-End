cc2f863c1dca84081a1bbd0e3e8a6e55bb6bf45e
ElyPoli <itsos.poli@gmail.com>
2023-11-03 11:29:20 +0100

    create component "PromoSlider"

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,17 @@
 <script>
+import PromoSlider from '../components/PromoSlider.vue';
 import StrengthsSection from '../components/StrengthsSection.vue';
 
 export default {
     components: {
+        PromoSlider,
         StrengthsSection
     },
 }
 </script>
 
 <template>
+    <PromoSlider></PromoSlider>
     <StrengthsSection></StrengthsSection>
 </template>
 
