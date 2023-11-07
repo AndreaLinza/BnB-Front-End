5d6c65a6d2e0c5d3d00857f126696b9eb4c57b3b
AndreaLinza <andrealinza@gmail.com>
2023-11-07 19:52:03 +0100

    add component TopOffcanvas

---
+++ src/components/partials/TopOffcanvas.vue
@@ -0,0 +1,51 @@
+<script>
+import Filters from '../advanced_research/Filters.vue';
+
+
+export default {
+    data() {
+        return {};
+    },
+    components: { Filters }
+}
+
+</script>
+
+<template>
+    <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
+        <div class="offcanvas-header container">
+            <h5 class="offcanvas-title text-white mx-3" id="offcanvasTopLabel">Filtri avanzati</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"><i
+                    class="fa-solid fa-xmark"></i></button>
+        </div>
+        <div class="offcanvas-body container d-flex flex-wrap">
+            <Filters></Filters>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+@use '../../style/partials/variables' as *;
+
+.offcanvas.offcanvas-top {
+    height: 60vh;
+}
+
+// =====================
+
+.offcanvas-top {
+    background-color: transparent;
+    border-bottom: none !important;
+
+    .offcanvas-header,
+    .offcanvas-body {
+        background-color: $secondary-color;
+    }
+
+    .btn-close {
+        background-image: none;
+        color: white;
+    }
+}
+</style>
