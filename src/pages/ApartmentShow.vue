2fec249bfba646b4ab8e492e222b9ba67e1e8b27
giuliacrr <giulia.corrado02@gmail.com>
2023-11-07 18:32:08 +0100

    apartment show basic layout

--- src/pages/ApartmentShow.vue
+++ src/pages/ApartmentShow.vue
@@ -33,33 +33,88 @@ export default {
 <template>
     <div class="container">
         <!--Banner appartamento-->
-        <div class="show-img small-pres text-center">
-            <img :src="fetchImage(apartment)" class="img-fluid" alt="{{ apartment.slug }}">
+        <div class="apt-pres map-border">
+            <img :src="fetchImage(apartment)" class="img-fluid show-img" alt="{{ apartment.slug }}">
         </div>
-        <!--Nome appartamento-->
-        <div class="infos small-pres">
-            <h1>{{ apartment.title }}</h1>
+        <!--Info apt + form contatto-->
+        <div class="row">
+            <!--Info generali-->
+            <div class="col-12 col-lg-9">
+                <div class="infos mt-3">
+                    <h2>{{ apartment.title }}</h2>
+                    <span class="aubergine">{{ apartment.address }}</span>
+                    <!--informazioni tecniche-->
+                    <h3 class="mt-3">Informazioni generali</h3>
+                    <span class="aubergine">
+                        {{ apartment.square_meters }} m², {{ apartment.rooms_number }} stanze, {{ apartment.beds_number }}
+                        letti,
+                        {{ apartment.bathrooms_number }} bagni.
+                    </span>
+                    <h3 class="mt-3">Servizi</h3>
+                    <span class="badge bg-secondary my-1 mx-1 text-white" v-for="service in apartment.services">{{
+                        service.title
+                    }}</span>
+                </div>
+            </div>
+            <!--form-->
+            <div class="col-12 col-lg-3">
+                <ContactForm></ContactForm>
+            </div>
         </div>
-
-        <!--informazioni appartamento-->
-
-
+        <!--mappa-->
+        <h3 class="mt-5 text-center">Ti troverai qui:</h3>
+        <div class="map-border">
+            <div class="mappa container-fluid">
+            </div>
+        </div>
         <!--Form di contatto-->
-        <ContactForm></ContactForm>
+
     </div>
 </template>
 
 <style lang="scss" scoped>
 @use '../style/partials/variables' as *;
 
+h2,
+h3,
+span {
+    color: $grey;
+}
+
+.infos {
+    padding: 10px;
+}
+
+
+
 .apt-pres {
-    max-height: fit-content;
+    height: 500px;
+    padding: 15px;
 }
 
 .show-img {
-    max-width: 100%;
+    width: 100%;
+    height: 100%;
     object-fit: cover;
-    margin-right: 30px;
+    object-position: center;
+}
+
+.map-border {
+    padding: 15px;
+    background-color: $secondary-color;
+    border: 2px solid $partial-secondary-color;
+}
+
+.mappa {
+    background-image: url(https://ostellobello.com/wp-content/uploads/2022/02/mappa.png);
+    width: 100%;
+    height: 200px;
+    background-position: center;
+}
+
+.bg-secondary {
+    background-color: $grey !important;
+    border: 2px solid $partial-secondary-color;
 }
 </style>
 
