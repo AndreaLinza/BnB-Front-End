77864a7400be3b01203201b427685b31fd557d56
giuliacrr <giulia.corrado02@gmail.com>
2023-11-07 15:22:47 +0100

    Created form component + show page

--- src/pages/ApartmentShow.vue
+++ src/pages/ApartmentShow.vue
@@ -1,7 +1,9 @@
 <script>
-
+import ContactForm from '../components/ContactForm.vue'
 export default {
-
+    components: {
+        ContactForm,
+    },
     data() {
         return {
 
@@ -14,27 +16,11 @@ export default {
 
 <template>
     <div class="container">
-        <div id="carouselExampleFade" class="carousel slide carousel-fade">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="..." class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="..." class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="..." class="d-block w-100" alt="...">
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div>
+        <!--Banner appartamento-->
+        <!--Informazioni Appartamento-->
+        <h1>Titolo Appartamento</h1><!---->
+
+        <ContactForm></ContactForm>
     </div>
 </template>
 
