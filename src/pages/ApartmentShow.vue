a8eed095f2b03182f0aa8cbb2cfcabbe00f25147
giuliacrr <giulia.corrado02@gmail.com>
2023-11-07 17:19:13 +0100

    Added axios call + storage img for apartment show

--- src/pages/ApartmentShow.vue
+++ src/pages/ApartmentShow.vue
@@ -1,24 +1,48 @@
 <script>
+import axios from "axios";
 import ContactForm from '../components/ContactForm.vue';
+
 export default {
     components: {
         ContactForm,
     },
     data() {
         return {
-
-        }
+            apartment: {},
+        };
+    },
+    methods: {
+        fetchApartment() {
+            axios.get("http://127.0.0.1:8000/api/apartments/" + this.$route.params.slug)
+                .then((response) => { this.apartment = response.data });
+            console.log(this.apartment);
+        },
+        fetchImage(apartment) {
+            return `http://127.0.0.1:8000/storage/${apartment.thumbnail}`;
+        },
+    },
+    mounted() {
+        this.fetchApartment();
+        console.log(this.apartment);
     }
-}
 
+}
 </script>
 
 
 <template>
     <div class="container">
         <!--Banner appartamento-->
-        <!--Informazioni Appartamento-->
-        <h1>Titolo Appartamento</h1><!---->
+        <div class="show-img small-pres text-center">
+            <img :src="fetchImage(apartment)" class="img-fluid" alt="{{ apartment.slug }}">
+        </div>
+        <!--Nome appartamento-->
+        <div class="infos small-pres">
+            <h1>{{ apartment.title }}</h1>
+        </div>
+
+        <!--informazioni appartamento-->
+
 
         <!--Form di contatto-->
         <ContactForm></ContactForm>
@@ -27,5 +51,15 @@ export default {
 
 <style lang="scss" scoped>
 @use '../style/partials/variables' as *;
+
+.apt-pres {
+    max-height: fit-content;
+}
+
+.show-img {
+    max-width: 100%;
+    object-fit: cover;
+    margin-right: 30px;
+}
 </style>
 
--- src/store.js
+++ src/store.js
@@ -9,7 +9,7 @@ export const store = reactive({
 });
 
 export function fetchApartments(filter) {
-  axios.get('http://127.0.0.1:8000/api/apartments' , {params:filter} ).then((response) => {
+  axios.get('http://127.0.0.1:8000/api/apartments', { params: filter }).then((response) => {
     store.apartments = response.data.apartments;
     console.log(store.apartments);
   })
@@ -20,3 +20,16 @@ export function fetchServices() {
     store.services = response.data.services;
   })
 }
+
+// export function fetchSingleApartment() {
+//   axios.get('http://127.0.0.1:8000/api/apartments/', + this.$route.params.slug)
+//     .then((response) => { this.singleApartment = response.data.apartments })
+//   // .catch((e) => {
+//   //   console.error(e);
+//   //   this.$router.push({ name: "not-found" });
+//   // })
+// }
+
+// export function fetchImageApartment(apartment) {
+//   return `http://127.0.0.1:8000/storage/${apartment.thumbnail}`;
+// }
