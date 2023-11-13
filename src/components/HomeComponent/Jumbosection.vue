9e8a241c2137196573bac1d86c86ddd550c5b8a3
AndreaLinza <andrealinza@gmail.com>
2023-11-13 18:30:07 +0100

    center text and change gradient

--- src/components/HomeComponent/Jumbosection.vue
+++ src/components/HomeComponent/Jumbosection.vue
@@ -16,8 +16,8 @@ export default {
     <section>
 
         <div class="jumbo">
-            <div class="container ">
-                <h3>Benvenuti sul nostro sito</h3>
+            <div class="container content">
+                <h2 class="fw-bold display-3">Benvenuti sul nostro sito</h2>
             </div>
         </div>
         <img src="../../../img/pexels-binyamin-mellish-106399.jpg" alt="">
@@ -39,7 +39,7 @@ section {
         position: absolute;
         z-index: 5;
         background: rgb(229, 88, 18);
-        background: linear-gradient(138deg, rgba(229, 88, 18, 0.58) 0%, rgba(22, 105, 122, 0.53) 35%, rgba(214, 211, 200, 0.24) 100%);
+        background: linear-gradient(180deg, rgba(229, 88, 18, 0.58) 0%, rgba(22, 105, 122, 0.53) 85%, rgba(214, 211, 200, 0.24) 100%);
         left: 0px;
         top: 0px;
         width: 100%;
@@ -47,9 +47,13 @@ section {
         display: inline-block;
         overflow: hidden;
 
-
-        h3 {
-            margin-top: 25%;
+        .content{
+            display: flex;
+            align-items: center;
+            height: 100%;
+            h2{
+                color: rgba(255, 255, 255, 0.699);
+            }
         }
 
 
