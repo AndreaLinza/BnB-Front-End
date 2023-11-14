9fb0bd8a965a94e33dbaa9b3f0d1293776681015
Pietro-Leone <leonepietro1999@gmail.com>
2023-11-14 01:15:50 +0100

    import Map in ApartmentShow

--- src/pages/ApartmentShow.vue
+++ src/pages/ApartmentShow.vue
@@ -43,67 +43,68 @@ export default {
 <template>
   <TopOffcanvas></TopOffcanvas>
 
-    <div class="container apartment-show-box margin-top-custom">
-        <!--Banner appartamento-->
-        <div class="apt-pres map-border">
-            <img :src="fetchImageApartment(apartment)" class="img-fluid show-img" alt="{{ apartment.slug }}">
-        </div>
-        <!--Info apt + form contatto-->
-        <div class="row gy-4 mb-5">
-            <!--Info generali-->
-            <div class="col-12 col-lg-7">
-                <div class="infos mt-3 h-100">
-                    <div class="mb-4">
-                        <h2 class="text-white">{{ apartment.title }}</h2>
-                        <small class="text-white">{{ apartment.address }}</small>
-                    </div>
-                    <!--informazioni tecniche-->
-                    <div class="mb-3">
-                        <h3 class="mt-3 text-white">Informazioni generali</h3>
-                        <span class="aubergine text-white">
-                            L'appartamento ha un'area di {{ apartment.square_meters }} m², comprende {{
-                                apartment.rooms_number }}
-                            stanze con {{ apartment.beds_number
-                            }}
-                            letti e
-                            {{ apartment.bathrooms_number }} bagni.<br>
-                            Tutti i servizi messi a disposizione dall'Host e il suo appartamento sono segnalati nella
-                            sezione sottostante "Servizi".<br>
-                            E' possibile anche visualizzare la posizione dell'immobile sulla
-                            <a href="#mappa" class="text-white">mappa</a> sottostante!
-                        </span>
-                    </div>
-                    <h3 class="mt-3 text-white">Servizi</h3>
-                    <span class="badge bg-secondary my-1 mx-1 text-white" v-for="service in apartment.services">{{
-                        service.title
-                    }}</span>
-                </div>
-            </div>
-            <!--Form di contatto-->
-            <div class="col-12 col-lg-5">
-                <ContactForm :apartment_id="apartment.id"></ContactForm>
-            </div>
-        </div>
-        <!--mappa-->
-        <div id="mappa">
-            <h3 class="mt-5 mb-4 text-center text-white">Ti troverai qui:</h3>
-            <div class="map-border">
-                <div class="mappa container-fluid">
-                </div>
-            </div>
-        </div>
+  <div class="container apartment-show-box margin-top-custom">
+    <!--Banner appartamento-->
+    <div class="apt-pres map-border">
+      <img :src="fetchImageApartment(apartment)" class="img-fluid show-img" alt="{{ apartment.slug }}">
     </div>
+    <!--Info apt + form contatto-->
+    <div class="row gy-4 mb-5">
+      <!--Info generali-->
+      <div class="col-12 col-lg-7">
+        <div class="infos mt-3 h-100">
+          <div class="mb-4">
+            <h2 class="text-white">{{ apartment.title }}</h2>
+            <small class="text-white">{{ apartment.address }}</small>
+          </div>
+          <!--informazioni tecniche-->
+          <div class="mb-3">
+            <h3 class="mt-3 text-white">Informazioni generali</h3>
+            <span class="aubergine text-white">
+              L'appartamento ha un'area di {{ apartment.square_meters }} m², comprende {{
+                apartment.rooms_number }}
+              stanze con {{ apartment.beds_number
+              }}
+              letti e
+              {{ apartment.bathrooms_number }} bagni.<br>
+              Tutti i servizi messi a disposizione dall'Host e il suo appartamento sono segnalati nella
+              sezione sottostante "Servizi".<br>
+              E' possibile anche visualizzare la posizione dell'immobile sulla
+              <a href="#mappa" class="text-white">mappa</a> sottostante!
+            </span>
+          </div>
+          <h3 class="mt-3 text-white">Servizi</h3>
+          <span class="badge bg-secondary my-1 mx-1 text-white" v-for="service in apartment.services">{{
+            service.title
+          }}</span>
+        </div>
+      </div>
+      <!--Form di contatto-->
+      <div class="col-12 col-lg-5">
+        <ContactForm :apartment_id="apartment.id"></ContactForm>
+      </div>
+    </div>
+    <!--mappa-->
+    <div id="mappa">
+      <h3 class="mt-5 mb-4 text-center text-white">Ti troverai qui:</h3>
+      <div class="map-border">
+        <div class="mappa container-fluid">
+          <Map :location="[apartment.longitude, apartment.latitude]"></Map>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 @use '../style/partials/variables' as *;
 
 .margin-top-custom {
-    margin-top: 150px;
+  margin-top: 150px;
 }
 
 #mappa {
-    margin-top: 100px;
+  margin-top: 100px;
 }
 
 .apartment-show-box {
@@ -112,16 +113,16 @@ export default {
 
 
 h2 {
-    font-size: 3rem;
+  font-size: 3rem;
 }
 
 h2,
 h3 {
-    margin-bottom: 0px;
+  margin-bottom: 0px;
 }
 
 small {
-    color: $primary-color !important;
+  color: $primary-color !important;
 }
 
 h2,
@@ -132,10 +133,10 @@ span {
 
 
 .infos {
-    padding: 20px;
-    background-color: #e99f8343;
-    border-radius: 20px;
-    border: 2px dashed white;
+  padding: 20px;
+  background-color: #e99f8343;
+  border-radius: 20px;
+  border: 2px dashed white;
 
 }
 
@@ -158,14 +159,14 @@ span {
 }
 
 .mappa {
-    background-image: url(https://ostellobello.com/wp-content/uploads/2022/02/mappa.png);
-    width: 100%;
-    height: 300px;
-    background-position: center;
+  background-image: url(https://ostellobello.com/wp-content/uploads/2022/02/mappa.png);
+  width: 100%;
+  height: 300px;
+  background-position: center;
 }
 
 .bg-secondary {
-    background-color: $secondary-color !important;
-    border: 2px solid $partial-secondary-color;
+  background-color: $secondary-color !important;
+  border: 2px solid $partial-secondary-color;
 }
 </style>
